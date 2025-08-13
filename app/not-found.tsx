82463c38a12ba7447ee432579e2854779c5f7cb1
Alena Havalehko <126282451+AlenaHavaleshko@users.noreply.github.com>
2025-08-14 00:28:01 +0300

    1 part

---
+++ app/not-found.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+// Варіант 1 з перенаправленням - Вас буде перенаправлено на головну через кілька секунд…
+import type { Metadata } from "next";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "404 - Page Not Found | NoteHub",
+  description: "The page you're looking for does not exist on NoteHub.",
+    openGraph: {
+    title: "404 - Page Not Found | NoteHub",
+    description: "The page you're looking for does not exist on NoteHub.",
+    url: "https://notehub.com/not-found",
+    images: [
+  {
+    url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+    width: 1200,
+    height: 630,
+    alt: "NoteHub 404 Page",
+  },
+],
+  },
+};
+
+const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Редірект через 3 секунди
+    const timer = setTimeout(() => router.push("/"), 3000);
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  return (
+    <div>
+      <h1>404 - Сторінку не знайдено</h1>
+      <p>Вас буде перенаправлено на головну через кілька секунд…</p>
+    </div>
+  );
+};
+
+export default NotFound;
---
+++ app/notes/filter/@sidebar/default.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import {tags} from "@/types/note"
+import css from "./SidebarNotes.module.css";
+
+// type SidebarNotesProps = {
+//   tags: string[];
+// };
+
+const NotesSidebar = async () => {
+
+  return (
+    <div>
+      <ul className={css.menuList}>
+        {/* список тегів */}
+        {tags.map((tag) => (
+          <li key={tag} className={css.menuItem}>
+            <Link href={`/notes/filter/${tag}`} className={css.menuLink}>
+              {tag}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default NotesSidebar;
